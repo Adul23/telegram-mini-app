ac60475965af5f17ab1150ad1ae2de38749ab52a
Adul23 <adilgg672@gmail.com>
2024-09-28 23:45:11 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, {useEffect} from 'react'; 
 import './App.css';
-import { useTelegram } from 'src/hooks/components/useTelegram.js';
+// import { useTelegram } from 'src/hooks/components/useTelegram.js';
 
 function App() {
-  const {onToggleButton, tg} = useTelegram();
+  // const {onToggleButton, tg} = useTelegram();
   useEffect(() => {
     tg.ready();
     
   }, [])
-  
+  const onToggleButton = () => {
+    if(tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else{
+        tg.MainButton.show();
+    }
+}
   return (
     <div className="App">
       <button onClick={onToggleButton}>toggle</button>
