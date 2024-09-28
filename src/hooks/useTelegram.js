c6f2b4401a5b65acfc05067e7afccf9132f71164
Adul23 <adilgg672@gmail.com>
2024-09-29 00:48:09 +0500

    init

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -2,15 +2,18 @@ const tg = window.Telegram.WebApp;
 
 export function useTelegram(){
     const onClose = () => {
+        
         tg.close()
     }
     const onToggleButton = () => {
+        tg.enableClosingConfirmation();
         if(tg.MainButton.isVisible) {
             tg.MainButton.hide();
         } else{
             tg.MainButton.show();
         }
     }
+    
     return {
         onClose,
         tg,
