9916791d09a83c05503bdb1d02a789d992409a54
Adul23 <adilgg672@gmail.com>
2024-10-18 22:48:47 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-import { useTelegram } from './hooks/useTelegram';
-import logo from './components/photos/logo.png'
-import finder from './components/photos/Layer 6.png'
-import back from './components/photos/Rectangle 7.png'
-import {Howl, Howler} from 'howler';
+import logo from './components/photos/logo.png';
+import finder from './components/photos/Layer 6.png';
+import back from './components/photos/Rectangle 7.png';
+import back1 from './components/photos/Rectangle 3886.png';
+import { Howl, Howler } from 'howler';
+
 function App() {
     return (
-        
         <div className="App">
-{/*             
-            {tg.user && <p>Hello, {tg.initDataUnsafe?.user?.username}!</p>} */}
-            <div class='header'>
-                <div class='catalog'>
-                    <img src={finder} alt ="Finder"/>
+            {/* Uncomment this if you want to show a greeting */}
+            {/* {tg.user && <p>Hello, {tg.initDataUnsafe?.user?.username}!</p>} */}
+            <div className="header">
+                <div className="catalog">
+                    <img src={finder} alt="Finder Icon" />
                 </div>
-                <div class = 'nameTeam'>
+                <div className="nameTeam">
                     <h12>Reactive JStars</h12>
-                    <div class = 'back-23'>
-                    <a href="#"> 
-                        <img src={back} alt=""/>
-                    </a>
-                    </div>
                 </div>
-                
-
+            </div>
+            <div className="back23">
+                <a href="#">
+                    <img src={back} alt="Back Navigation" />
+                </a>
+            </div>
+            <div className="back233">
+                <a href="#">
+                    <img src={back1} alt="Additional Navigation" />
+                </a>
             </div>
         </div>
     );
