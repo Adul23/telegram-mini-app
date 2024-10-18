da5093e6af2f52dcf6d4143f94e142b55629999f
Adul23 <adilgg672@gmail.com>
2024-10-18 23:16:40 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import logo from './components/photos/logo.png';
+import logo from './components/photos/logo.png'; // If not used, consider removing
 import finder from './components/photos/Layer 6.png';
 import back from './components/photos/Rectangle 7.png';
 import back1 from './components/photos/Rectangle 3886.png';
-import { Howl, Howler } from 'howler';
 
 function App() {
     return (
@@ -16,17 +15,17 @@ function App() {
                     <img src={finder} alt="Finder Icon" />
                 </div>
                 <div className="nameTeam">
-                    <h12>Reactive JStars</h12>
+                    <h2>Reactive JStars</h2>
                 </div>
             </div>
             <div className="back23">
-                <a href="#">
-                    <img src={back} alt="Back Navigation" />
+                <a href="#" aria-label="Back Navigation">
+                    <img src={back} alt="Back Navigation Button" />
                 </a>
             </div>
             <div className="back233">
-                <a href="#">
-                    <img src={back1} alt="Additional Navigation" />
+                <a href="#" aria-label="Additional Navigation">
+                    <img src={back1} alt="Additional Navigation Button" />
                 </a>
             </div>
         </div>
