7d1978e898f44804ff2c639fb470e9635a9f08f2
Adul23 <adilgg672@gmail.com>
2024-10-18 23:26:48 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,76 @@ import logo from './components/photos/logo.png'; // If not used, consider removi
 import finder from './components/photos/Layer 6.png';
 import back from './components/photos/Rectangle 7.png';
 import back1 from './components/photos/Rectangle 3886.png';
-
+const RegistrationForm = () => {
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [email, setEmail] = useState('');
+    const  
+   [password, setPassword] = useState('');  
+  
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // Handle form submission here (e.g., send data to server)
+      console.log('Form submitted:',  
+   { name, surname, email, password });
+    };
+  
+    return (
+      <div className="registration-form">
+        <h2>Registration</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>  
+  
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="surname">Surname</label>  
+  
+            <input
+              type="text"
+              id="surname"
+              value={surname}
+              onChange={(e) => setSurname(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">  
+  
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}  
+  
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"  
+  
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Sign up</button>
+        </form>
+      </div>
+    );
+  };
+  
 function App() {
     return (
         <div className="App">
