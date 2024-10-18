b59961a8366bc06e9654ebdef8a1f739e4ed4da2
Adul23 <adilgg672@gmail.com>
2024-10-18 22:10:28 +0500

    third

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useTelegram } from './hooks/useTelegram';
 import logo from './components/photos/logo.png'
 import finder from './components/photos/Layer 6.png'
+import back from './components/photos/Rectangle 7.png'
 import {Howl, Howler} from 'howler';
 function App() {
     return (
@@ -13,10 +14,17 @@ function App() {
             <div class='header'>
                 <div class='catalog'>
                     <img src={finder} alt ="Finder"/>
-                </div>
+                
                 <div class = 'nameTeam'>
                     <h12>Reactive JStars</h12>
+                    <div class = 'back-23'>
+                    <a href="#"> 
+                        <img src={back} alt=""/>
+                    </a>
                 </div>
+                </div>
+                </div>
+                
 
             </div>
         </div>
