9a2e54e1d369178a478e6aba7dee9831b41b26cf
Adul23 <adilgg672@gmail.com>
2024-10-19 13:40:29 +0500

    somechanges

--- src/App.js
+++ src/App.js
@@ -1,77 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const RegistrationForm = () => {
-    const [name, setName] = useState('');
-    const [surname, setSurname] = useState('');
-    const [email, setEmail] = useState('');
-    const  
-   [password, setPassword] = useState('');  
-  
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Handle form submission here (e.g., send data to server)
-      console.log('Form submitted:',  
-   { name, surname, email, password });
-    };
-  
-    return (
-      <div className="registration-form">
-        <h2>Registration</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>  
-  
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="surname">Surname</label>  
-  
-            <input
-              type="text"
-              id="surname"
-              value={surname}
-              onChange={(e) => setSurname(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">  
-  
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}  
-  
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"  
-  
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit">Sign up</button>
-        </form>
-      </div>
-    );
-  };
+
   
 function App() {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      // Handle form submission here (e.g., send data to server)
+      console.log('Form submitted:', { name, surname, email, password });
+  };
+
+  // return (
+  //     <div className="registration-form">
+  //         <h2>Registration</h2>
+  //         <form onSubmit={handleSubmit}>
+  //             <div className="form-group">
+  //                 <label htmlFor="name">Name</label>
+  //                 <input
+  //                     type="text"
+  //                     id="name"
+  //                     value={name}
+  //                     onChange={(e) => setName(e.target.value)}
+  //                     required
+  //                 />
+  //             </div>
+  //             <div className="form-group">
+  //                 <label htmlFor="surname">Surname</label>
+  //                 <input
+  //                     type="text"
+  //                     id="surname"
+  //                     value={surname}
+  //                     onChange={(e) => setSurname(e.target.value)}
+  //                     required
+  //                 />
+  //             </div>
+  //             <div className="form-group">
+  //                 <label htmlFor="email">Email</label>
+  //                 <input
+  //                     type="email"
+  //                     id="email"
+  //                     value={email}
+  //                     onChange={(e) => setEmail(e.target.value)}
+  //                     required
+  //                 />
+  //             </div>
+  //             <div className="form-group">
+  //                 <label htmlFor="password">Password</label>
+  //                 <input
+  //                     type="password"
+  //                     id="password"
+  //                     value={password}
+  //                     onChange={(e) => setPassword(e.target.value)}
+  //                     required
+  //                 />
+  //             </div>
+  //             <button type="submit">Sign up</button>
+  //         </form>
+  //     </div>
+  // );
     return (
         <div className="App">
             {/* {tg.user && <p>Hello, {tg.initDataUnsafe?.user?.username}!</p>} */}
@@ -107,12 +98,28 @@ function App() {
                     Name
                     
                   </div>
+                  <input className="Name"
+                      type="text"
+                      id="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      
+                      required
+                  />
                 <div className = 'pole'>
-              
+
                   </div>
                   <div className = "textName">
                     Surname
+                    
                   </div>
+                  <input className="Surname"
+                      type="text"
+                      id="surname"
+                      value={surname}
+                      onChange={(e) => setSurname(e.target.value)}
+                      required
+                  />
                   <div className = 'pole'>
                 
                   </div>
